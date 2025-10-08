0c041046448a7c7e91b2cb9d4e45dc2aa099fbe4
anrmathe <anrmathe@ucsc.edu>
2025-10-08 23:40:31 +0000

    Added remote control image as button (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import remoteURL from "./remote-control-vector-isolated-icon-remote-control-emoji-illustration-remote-control-vector-icon_603823-875.jpg";
 import "./style.css";
 
 let counter: number = 0;
@@ -5,12 +6,23 @@ let counter: number = 0;
 // Create basic HTML structure
 document.body.innerHTML = `
   <h1>CMPM 121 Project</h1>
-  <p>Counter: <span id="counter">0</span></p>
-  <button id="increment">Click Me!</button>
+  <p>Total Attention Units: <span id="counter">0</span></p>
+  <button id="increment"></button>
+  <p>Example image asset: <img src="${remoteURL}" class="icon" /></p>
 `;
 
 // Add click handler
 const button = document.getElementById("increment")!;
+
+const img = document.createElement("img");
+img.src = remoteURL; // <-- put your image path here
+img.alt = "Click to change channels!";
+img.width = 128;
+img.height = 128;
+
+// Add the image inside the button
+button.appendChild(img);
+
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
