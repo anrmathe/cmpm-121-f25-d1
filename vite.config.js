d0b74882b9b3db5ab626a69241f819d3db0f15ab
anrmathe <anrmathe@ucsc.edu>
2025-10-08 09:36:36 +0000

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // https://vitejs.dev/config/
 export default {
-  base: Deno.env.get("REPO_NAME") || "/project",
+  base: Deno.env.get("/cmpm-121-f25-d1/") || "/project",
   server: {
     port: 3000,
     open: true,
