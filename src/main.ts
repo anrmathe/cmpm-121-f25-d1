f8a96fa018b05e25a3323e825a08e1ff9e098abd
anrmathe <anrmathe@ucsc.edu>
2025-10-10 05:15:27 +0000

    Automatic clicking (step 3 complete)

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ document.body.innerHTML = `
   <h1>CMPM 121 Project</h1>
   <p>Total Attention Units: <span id="counter">0</span></p>
   <button id="increment"></button>
+  <button id="autoclick-btn">Enable Channel Surfer</button>
   <p>Example image asset: <img src="${remoteURL}" class="icon" /></p>
 `;
 
 // Add click handler
 const button = document.getElementById("increment")!;
+const autoclickButton = document.getElementById("autoclick-btn")!;
 
 const img = document.createElement("img");
 img.src = remoteURL; // <-- put your image path here
@@ -30,6 +32,18 @@ button.addEventListener("click", () => {
   ++counter;
   counterElement.textContent = counter.toString();
   // console.log("I have these thingies:", button, counterElement, counter);
+});
+
+let autoclickEnabled = false;
+
+autoclickButton.addEventListener("click", () => {
+  autoclickEnabled = !autoclickEnabled;
+  if (autoclickEnabled) {
+    setInterval(() => {
+      ++counter;
+      counterElement.textContent = counter.toString();
+    }, 1000);
+  }
 });
 
 console.log("cookie");
